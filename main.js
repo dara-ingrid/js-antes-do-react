510a841a310197f1e1c6a3e1886a7ba83a604032
Dara Ferreira <dara-ingrid@hotmail.com>
2023-07-28 13:36:58 -0300

    Default Export

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import { soma, subtracao, PI } from "./lib/math";
+import sum from './lib/sum'
 
+//Named export
 console.log(soma(1, 3))
 console.log(subtracao(4, 2))
 console.log(PI)
 
-//Named export
+//Default export - Declara a função em outro arquivo como defalt, +
+//+ e pode importar aqui sem precisar colocar entre chaves, e com outro nome.
+console.log(sum(3, 3))
